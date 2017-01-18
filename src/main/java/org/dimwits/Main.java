3e897d716d0af5a1cce5df452051c5a551175010
arid1995 <arid1995@mail.ru>
2017-01-18 17:52:36 +0300

    initial commit

---
+++ src/main/java/org/dimwits/Main.java
@@ -0,0 +1,12 @@
+package org.dimwits;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by farid on 1/18/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
